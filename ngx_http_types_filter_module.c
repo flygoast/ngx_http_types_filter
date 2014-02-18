555502042bbbbcb4972104a778bd3dc4f690b27f
flygoast <flygoast@126.com>
2014-02-18 16:42:13 +0800

    bugfix: no end opcode added in code engine

--- ngx_http_types_filter_module.c
+++ ngx_http_types_filter_module.c
@@ -459,7 +459,7 @@ ngx_http_types_filter_condition(ngx_conf_t *cf,
         }
 
         if (cur == last) {
-            return NGX_CONF_OK;
+            goto end;
         }
 
         cur++;
@@ -483,7 +483,7 @@ ngx_http_types_filter_condition(ngx_conf_t *cf,
 
             *code = ngx_http_script_equal_code;
 
-            return NGX_CONF_OK;
+            goto end;
         }
 
         if (len == 2 && p[0] == '!' && p[1] == '=') {
@@ -502,7 +502,7 @@ ngx_http_types_filter_condition(ngx_conf_t *cf,
             }
 
             *code = ngx_http_script_not_equal_code;
-            return NGX_CONF_OK;
+            goto end;
         }
 
         if ((len == 1 && p[0] == '~')
@@ -538,7 +538,7 @@ ngx_http_types_filter_condition(ngx_conf_t *cf,
             }
             regex->name = value[last];
 
-            return NGX_CONF_OK;
+            goto end;
         }
 
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
@@ -573,43 +573,43 @@ ngx_http_types_filter_condition(ngx_conf_t *cf,
 
         if (p[1] == 'f') {
             fop->op = ngx_http_script_file_plain;
-            return NGX_CONF_OK;
+            goto end;
         }
 
         if (p[1] == 'd') {
             fop->op = ngx_http_script_file_dir;
-            return NGX_CONF_OK;
+            goto end;
         }
 
         if (p[1] == 'e') {
             fop->op = ngx_http_script_file_exists;
-            return NGX_CONF_OK;
+            goto end;
         }
 
         if (p[1] == 'x') {
             fop->op = ngx_http_script_file_exec;
-            return NGX_CONF_OK;
+            goto end;
         }
 
         if (p[0] == '!') {
             if (p[2] == 'f') {
                 fop->op = ngx_http_script_file_not_plain;
-                return NGX_CONF_OK;
+                goto end;
             }
 
             if (p[2] == 'd') {
                 fop->op = ngx_http_script_file_not_dir;
-                return NGX_CONF_OK;
+                goto end;
             }
 
             if (p[2] == 'e') {
                 fop->op = ngx_http_script_file_not_exists;
-                return NGX_CONF_OK;
+                goto end;
             }
 
             if (p[2] == 'x') {
                 fop->op = ngx_http_script_file_not_exec;
-                return NGX_CONF_OK;
+                goto end;
             }
         }
 
@@ -622,6 +622,17 @@ ngx_http_types_filter_condition(ngx_conf_t *cf,
                        "invalid condition \"%V\"", &value[cur]);
 
     return NGX_CONF_ERROR;
+
+end:
+
+    code = ngx_array_push_n(condition->codes, sizeof(uintptr_t));
+    if (code == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    *code = (uintptr_t) NULL;
+
+    return NGX_CONF_OK;
 }
 
 
